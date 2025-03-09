5e4402fa8984470c3a921a6bf63059401143bc6b
Atif Galamov <kalemli@gmail.com>
2025-03-09 14:36:58 +0400

    Fixing minor issues

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
 export function connectToMongoDB() {
-  const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/preply";
+  const MONGO_URI = process.env.MONGO_URI;
+  if (!MONGO_URI) {
+    console.error(
+      "MongoDB connection error: MONGO_URI not specified as env. variable"
+    );
+    return;
+  }
   mongoose
     .connect(MONGO_URI)
     .then(() => console.log("MongoDB connected"))
